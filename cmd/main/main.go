cf6667977c41a6144aea5cd77bbf7dc402094894
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-09-19 14:04:38 +0200

    print upcoming weather to CLI

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
+
+type Forecast struct {
+	Hourly struct {
+		Time        []string  `json:"time"`
+		Temperature []float64 `json:"temperature_2m"`
+	} `json:"hourly"`
+}
 
 func main() {
 	url := "https://api.open-meteo.com/v1/forecast?latitude=47.4984&longitude=19.0404&hourly=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1"
@@ -23,5 +32,24 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
+	var data Forecast
+
+	error := json.Unmarshal(body, &data)
+
+	if error != nil {
+		panic(error)
+	}
+
+	for i, value := range data.Hourly.Time {
+		t, _ := time.Parse("2006-01-02T15:04", value)
+
+		if t.Before(time.Now()) {
+			continue
+		}
+
+		line := fmt.Sprintf("%s: %.0f°", t.Format("15:04"), data.Hourly.Temperature[i])
+
+		fmt.Println(line)
+	}
+
 }
